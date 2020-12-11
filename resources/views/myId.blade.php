da41c2accf3770f72d40193c23cff7e5fa8a848b
AnasTrk <anastarak9@gmail.com>
2020-12-11 10:32:22 +0100

    NewCommit

---
+++ resources/views/myId.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyId</title>
+</head>
+<body>
+    <p>my id is {{ $id }}</p>
+</body>
+</html>
